2e14288190c25c77eff13a3a3d8b7277b89f5d91
sudipta bhowmik <sudipta.bhowmik@ext.airbnb.com>
2025-12-29 13:08:04 +0530

    initial sprint boot setup

---
+++ src/main/java/com/code_editor/interview_assesement/InterviewCodeAssesementApplication.java
@@ -0,0 +1,13 @@
+package com.code_editor.interview_assesement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCodeAssesementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewCodeAssesementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code_editor/interview_assesement/InterviewCodeAssesementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code_editor.interview_assesement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCodeAssesementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
